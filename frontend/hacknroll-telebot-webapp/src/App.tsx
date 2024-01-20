b94a1f62343c6722c6ca0c25967fdae9b2038469
linedoestrolling <yiheng.yh.zhao@gmail.com>
2024-01-20 14:30:11 +0800

    installed all dependencies

--- frontend/hacknroll-telebot-webapp/src/App.tsx
+++ frontend/hacknroll-telebot-webapp/src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Theme, Flex, Text} from '@radix-ui/themes';
+import { Button } from "@/components/ui/button"
+ 
+
 
 function App() {
   return (
-    <h1 className="text-red-600">
-      Hello world!
-    </h1>
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes</Text>
+      <div>
+      <Button>Click meme me</Button>
+      </div>
+    </Flex>
   )
 }
 export default App
