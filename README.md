7c551964576d220a2d7c9194900ef4a3fcdf576e
linedoestrolling <yiheng.yh.zhao@gmail.com>
2024-01-20 13:49:45 +0800

    added frontend

--- README.md
+++
@@ -1,1 +0,0 @@
-# hackNRoll-telebot
---
+++ frontend/hacknroll-telebot-webapp/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-red-600">
+      Hello world!
+    </h1>
+  )
+}
+export default App
