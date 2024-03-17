2554dbd1c6b1dd046caca51360f7ed0927620ddd
kaungzinye <154214243+kaungzinye@users.noreply.github.com>
2024-03-18 00:29:37 +0800

    add missing packages

--- hacknroll-frontend0/vite.config.ts
+++ hacknroll-frontend0/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
-  base: "/hacknroll-frontend0/",
+  base: "/hacknroll-frontend/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
