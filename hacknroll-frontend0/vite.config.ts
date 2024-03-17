0a169cbe54882f9d50e5bf0fb763344a974d1846
Jensen H <jensen@huangyankai.com>
2024-03-18 01:33:26 +0800

    Update vite.config.ts

--- hacknroll-frontend0/vite.config.ts
+++ hacknroll-frontend0/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
-  base: "/hacknroll-frontend/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
